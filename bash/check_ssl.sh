956eecfff7d7e0250190ec3c3d237c3722bba437
Brian Braunstein <bbraunstein@modusagency.com>
2017-10-11 11:32:51 -0400

    Adding check_ssl script

--- bash/check_ssl.sh
+++ bash/check_ssl.sh
@@ -1,40 +1,53 @@
 #!/bin/bash
 # {{{ Header
 #########################################################################################################
-# Title       : check_ssl_expiry.sh                                                                     #
+# Title       : check_ssl.sh                                                                            #
 # Description : Checks the expiration date on SSL certs and calculates days remaining                   #
 # Author      : Brian Braunstein <bbraunstein@modusagency.com>                                          #
 # Created     : 02-21-2017                                                                              #
-# Modified    : 02-21-2017                                                                              #
+# Modified    : 02-23-2017                                                                              #
+# Changes     :                                                                                         #
+#               * Moved away from curl and added `openssl s_client` as our test case                    #
+#               * Added test case to ensure a certificate is present, otherwise fail                    #
+#                                                                                                       #
 #########################################################################################################
 #}}}
 
 _curl=$(which curl)
 _grep=$(which grep)
 _awk=$(which awk)
+_sed=$(which sed)
 _date=$(which date)
 _e=$(which echo)
-_host=${1:?usage: $0 <server>}
+_host=${1:?usage: $0 <target_host> <target port>}
+_port=${2:-443}
 
-edate=$(${_curl} -sv ${_host} 2> >(${_grep} "expire date:" | ${_awk} '{print $4}'))
+edate=$(openssl s_client -connect ${_host}:${_port} 2>/dev/null </dev/zero| openssl x509 -noout -enddate | sed 's/notAfter=//')
+# Put in place to fail if no SSL certificate is configured.
+if [ -z "${edate}" ]
+then
+  ${_e} "No SSL cert found"
+  exit 3
+fi
+cn=$(openssl s_client -connect ${_host}:${_port} 2>/dev/null </dev/zero| openssl x509 -noout -subject | sed 's/subject= //')
 exp=$(${_date} --date="${edate}" +%s)
 now=$(${_date} +%s)
 result=$(((${exp} - ${now}) / 86400))
 
-if [ "${result}" -lt 7 ]
+if [ "${result}" -le 7 ]
   then
-    ${_e} "Critical: ${result} days remaining before cert expires!"
-    exit 3
-  elif [ "${result}" -lt 60 ] && [ "${result}" -gt 7 ]
-  then
-    ${_e} "Warning: ${result} days remaining before cert expires."
+    ${_e} "Critical: ${result} days remaining before cert expires! ${cn}"
     exit 2
-  elif [ "${result}" -gt 60 ]
+  elif [ "${result}" -lt 60 ]
   then
-    ${_e} "SSL Cert OK."
+    ${_e} "Warning: ${result} days remaining before cert expires - ${cn}"
+    exit 1
+  elif [ "${result}" -ge 60 ]
+  then
+    ${_e} "SSL Cert OK. Expiring on: ${edate}"
     exit 0
   else
-    ${_e} "Script ${0} error"
-    exit 1
+    ${_e} "Invalid host or script ${0} error."
+    exit 3
 fi
 
